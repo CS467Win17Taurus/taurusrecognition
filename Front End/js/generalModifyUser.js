8f6414f53d0229ad2474b3cc3bcb754cd1e295ff
Alicia B <abroederdorf@gmail.com>
2017-02-13 22:56:16 -0800

    Refactor modify user account code
    Pulled general functions into general js file, then have separate js
    files for modifying user in either user section or admin section.

---
+++ Front End/js/generalModifyUser.js
@@ -0,0 +1,133 @@
+/*Taurus Employee Recognition
+General functions to support front end
+General modify user account functions to support
+page in user section and admin section
+*/
+
+var userPw = "";
+var userId = -1;
+
+//Get department list
+function getDepts(){
+	makeRequest('GET', "http://138.197.7.194/api/divisions/", null, true, usersDeptResponse);
+}
+
+//Handle get depts response
+function usersDeptResponse(response){
+	var sel = document.getElementById("dept");
+	var opt = document.createElement("option");
+	response.forEach(function(type){
+		//Reference: http://stackoverflow.com/a/6194450
+		sel.add(new Option(type.name, type.did));
+	});
+	getFormData();
+}
+
+//Function to fill fields of form
+function fillForm(data){
+	document.getElementById("fName").value = data.fName;
+	document.getElementById("lName").value = data.lName;
+	document.getElementById("email").value = data.email;
+	document.getElementById("dept").value = data.dept;
+	document.getElementById("sigImg").src = data.signature;
+	userPw = data.password;
+	userId = data.id;
+}
+
+//Validate form
+function validateForm(){
+	var errorHTML = "";
+	var numErrors = 0;
+	
+	//Check that fields are not empty
+	if (document.getElementById('fName').value == ""){
+		errorHTML += "***Please inlcude a first name.";
+		numErrors++;
+	}
+	if (document.getElementById('lName').value == ""){
+		if (numErrors > 0)
+			errorHTML += "<br>";
+		errorHTML += "***Please inlcude a last name.";
+		numErrors++;
+	}
+	if (document.getElementById('email').value == ""){
+		if (numErrors > 0)
+			errorHTML += "<br>";
+		errorHTML += "***Please inlcude an email.";
+		numErrors++;
+	}
+	if (document.getElementById('dept').value == ""){
+		if (numErrors > 0)
+			errorHTML += "<br>";
+		errorHTML += "***Please select a department.";
+		numErrors++;
+	}
+	
+	if (numErrors > 0){
+		document.getElementById("formErrors").innerHTML = errorHTML;
+		document.getElementById("formErrors").className = "badStatus";
+		return false
+	}
+	else
+		return true;
+}
+
+//Validate password
+function validatePassword(){
+	var numErrors = 0;
+	var errorHTML = "";
+	
+	//Check old password is correct
+	if (document.getElementById("oldPw").value != userPw){
+		errorHTML += "***Current password is incorrect.";
+		numErrors++;
+	}
+	
+	//Check that new passwords match
+	if (document.getElementById("newPw").value != document.getElementById("newPw2").value){
+		if (numErrors > 0)
+			errorHTML += "<br>";
+		errorHTML += "***New passwords do not match.";
+		numErrors++;
+	}
+	
+	//Print any errors
+	if (numErrors > 0){
+		errorHTML += "<br>";
+		document.getElementById("pwordErrors").innerHTML = errorHTML;
+		document.getElementById("pwordErrors").className = "badStatus";
+		return false;
+	}
+	else
+		return true;
+}
+
+//Save Data
+function save(){
+	//Clear prior error messages
+	document.getElementById("formErrors").textContent = "";
+	document.getElementById("pwordErrors").textContent = "";
+	
+	if (validateForm()){
+		if (document.getElementById("newPw").value != ""){
+			if (validatePassword()){
+				sendData();
+			}
+		}
+		else
+			sendData();
+	}
+}
+
+//Ref: http://stackoverflow.com/a/901144
+function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
--- Front End/js/uModifyUser.js
+++ Front End/js/uModifyUser.js
@@ -15,122 +15,10 @@ function initializePage(){
 	}
 }
 
-//Get department list
-function getDepts(){
-	makeRequest('GET', "http://138.197.7.194/api/divisions/", null, true, usersDeptResponse);
-}
-
-//Handle get depts response
-function usersDeptResponse(response){
-	var sel = document.getElementById("dept");
-	var opt = document.createElement("option");
-	response.forEach(function(type){
-		//Reference: http://stackoverflow.com/a/6194450
-		sel.add(new Option(type.name, type.did));
-	});
-	getFormData();
-}
-
 //Get form data
 function getFormData(){
 	var id = getId('user_id');
 	makeRequest('GET', "http://138.197.7.194/api/users/?id=" + id, null, true, fillForm);
-}
-
-//Function to fill fields of form
-function fillForm(data){
-	document.getElementById("fName").value = data.fName;
-	document.getElementById("lName").value = data.lName;
-	document.getElementById("email").value = data.email;
-	document.getElementById("dept").value = data.dept;
-	document.getElementById("sigImg").src = data.signature;
-	userPw = data.password;
-	userId = data.id;
-}
-
-//Validate form
-function validateForm(){
-	var errorHTML = "";
-	var numErrors = 0;
-	
-	//Check that fields are not empty
-	if (document.getElementById('fName').value == ""){
-		errorHTML += "***Please inlcude a first name.";
-		numErrors++;
-	}
-	if (document.getElementById('lName').value == ""){
-		if (numErrors > 0)
-			errorHTML += "<br>";
-		errorHTML += "***Please inlcude a last name.";
-		numErrors++;
-	}
-	if (document.getElementById('email').value == ""){
-		if (numErrors > 0)
-			errorHTML += "<br>";
-		errorHTML += "***Please inlcude an email.";
-		numErrors++;
-	}
-	if (document.getElementById('dept').value == ""){
-		if (numErrors > 0)
-			errorHTML += "<br>";
-		errorHTML += "***Please select a department.";
-		numErrors++;
-	}
-	
-	if (numErrors > 0){
-		document.getElementById("formErrors").innerHTML = errorHTML;
-		document.getElementById("formErrors").className = "badStatus";
-		return false
-	}
-	else
-		return true;
-}
-
-//Validate password
-function validatePassword(){
-	var numErrors = 0;
-	var errorHTML = "";
-	
-	//Check old password is correct
-	if (document.getElementById("oldPw").value != userPw){
-		errorHTML += "***Current password is incorrect.";
-		numErrors++;
-	}
-	
-	//Check that new passwords match
-	if (document.getElementById("newPw").value != document.getElementById("newPw2").value){
-		if (numErrors > 0)
-			errorHTML += "<br>";
-		errorHTML += "***New passwords do not match.";
-		numErrors++;
-	}
-	
-	//Print any errors
-	if (numErrors > 0){
-		errorHTML += "<br>";
-		document.getElementById("pwordErrors").innerHTML = errorHTML;
-		document.getElementById("pwordErrors").className = "badStatus";
-		return false;
-	}
-	else
-		return true;
-}
-
-//Save Data
-function save(){
-	//Clear prior error messages
-	document.getElementById("formErrors").textContent = "";
-	document.getElementById("pwordErrors").textContent = "";
-	
-	if (validateForm()){
-		if (document.getElementById("newPw").value != ""){
-			if (validatePassword()){
-				sendData();
-			}
-		}
-		else
-			sendData();
-	}
 }
 
 //Send data
@@ -155,7 +43,8 @@ function sendData(){
 	if (document.getElementById("newPw").value != "")
 		data.append("password", document.getElementById("newPw").value);
 	
-	makeRequestFormData('PUT', "http://mockbin.org/bin/fbca1ddd-dc5d-4a4d-9640-bc4c0e4514e5", data, true, userSendDataResponse);
+	//makeRequestFormData('PUT', "http://mockbin.org/bin/fbca1ddd-dc5d-4a4d-9640-bc4c0e4514e5", data, true, userSendDataResponse);
+	makeRequestFormData('PUT', "http://138.197.7.194/api/users/", data, true, userSendDataResponse);
 }
 
 //Handle response after modifying user
