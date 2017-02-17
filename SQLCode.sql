0416b281857fd23707ce0a3188a49916d30f9ffd
esmjb <mjbonney78@gmail.com>
2017-02-17 10:07:10 -0600

    Update sqlcode.sql
    modify all tables with active column
    include insert into statements
    include userawards select statement

--- SQLCode.sql
+++ SQLCode.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS `division`;
 CREATE TABLE `division` (did INT(11) AUTO_INCREMENT PRIMARY KEY,
-                         name VARCHAR(30));
+                         name VARCHAR(30),
+                         active INT(7) DEFAULT 1,
+                         UNIQUE (name));
 
 INSERT INTO division (name) values ("accounting");
 INSERT INTO division (name) values ("sales");
@@ -16,13 +18,17 @@ CREATE TABLE users (id INT(11) AUTO_INCREMENT PRIMARY KEY,
                     password VARCHAR(70) NOT NULL,
                     timeCreated TIMESTAMP, 
                     signature VARCHAR(30), 
-                    dept INT(11),    
+                    dept INT(11),
+                    active INT(7) DEFAULT 1,                    
                     FOREIGN KEY (dept) REFERENCES division(did));
                     
 INSERT INTO users (fName, lName, email, password, timeCreated, dept) VALUES 
                   ("mike", "smith", "bonneym@oregonstate.edu", "msmith", current_timestamp, 1);
 INSERT INTO users (fName, lName, email, password, timeCreated, dept) VALUES 
-                  ("alicia", "broederdorf", "broedera@oregonstate.edu", "aliciapass", current_timestamp, 1);
+                  ("alicia", "broederdorf", "broedera@oregonstate.edu", "aliciapass", current_timestamp, 2);
+INSERT INTO users (fName, lName, email, password, timeCreated, dept, active) VALUES 
+                  ("fred", "garvin", "garvin@hotmail", "freddy", current_timestamp, 2, 0);
+                  
                   
 
 
@@ -30,14 +36,18 @@ INSERT INTO users (fName, lName, email, password, timeCreated, dept) VALUES
 DROP TABLE IF EXISTS `admins`;
 CREATE TABLE admins (id INT(11) AUTO_INCREMENT PRIMARY KEY,  
                      adminName VARCHAR(30) NOT NULL,
-                     password VARCHAR(30) NOT NULL);
+                     password VARCHAR(30) NOT NULL, 
+                     active INT(7) DEFAULT 1, 
+                     UNIQUE (adminName, password));
                      
 INSERT INTO admins (adminName, password) VALUES ("benbrew", "drspock");    
 INSERT INTO admins (adminName, password) VALUES ("mikejam", "nolimit");                 
                     
 DROP TABLE IF EXISTS `awards`;
 CREATE TABLE awards ( aid INT(11) AUTO_INCREMENT PRIMARY KEY,
-                      title VARCHAR(255) NOT NULL);
+                      title VARCHAR(255) NOT NULL,
+                      active INT(7) DEFAULT 1,
+                      UNIQUE (title));
                       
 INSERT INTO awards (title) values ("Employee of the Month");
 INSERT INTO awards (title) values ("Employee of the Week");
@@ -45,7 +55,9 @@ INSERT INTO awards (title) values ("New Sales Record");
                       
 DROP TABLE IF EXISTS `bonus`;
 CREATE TABLE bonus (bid INT(11) AUTO_INCREMENT PRIMARY KEY,
-                    amount INT(11) NOT NULL);
+                    amount INT(11) NOT NULL,
+                    active INT(7) DEFAULT 1,
+                    UNIQUE (amount));
                     
 INSERT INTO bonus (amount) values (25);
 INSERT INTO bonus (amount) values (50);
@@ -63,9 +75,10 @@ CREATE TABLE userAwards ( uaid INT(11) AUTO_INCREMENT PRIMARY KEY ,
                           FOREIGN KEY (awardID) REFERENCES awards(aid),
                           FOREIGN KEY (bonusID) REFERENCES bonus(bid));
 
+INSERT INTO userAwards (recipient, giver, awardID, bonusID, awardDate) VALUES (1,2,1,2, curdate());
+INSERT INTO userAwards (recipient, giver, awardID, bonusID, awardDate) VALUES (1,3,2,3, curdate());
+INSERT INTO userAwards (recipient, giver, awardID, bonusID, awardDate) VALUES (2,1,3,1, curdate());
 
-
-
-
-
-                    
+SELECT UA.uaid, t1.fName AS recipientFName, t1.lName AS recipientLName, t2.fName AS giverFName,
+t2.lName AS giverLName, UA.awardDate, title AS awardTitle, amount AS bonusAmount FROM userAwards UA join users t1 on UA.recipient=t1.id 
+join users t2 on UA.giver=t2.id INNER JOIN awards on UA.awardID=awards.aid INNER JOIN bonus on UA.bonusID=bonus.bid;
