662ee7fbfbef9227cb5b5a922b3064667903288a
Alicia B <abroederdorf@gmail.com>
2017-03-09 00:00:33 -0800

    Bug Fix: Display error if item already exists
    Since handling potential error messages in response or object in
    response, had to add functions for creating an item from a new input or
    from a reactivated entity.

--- Front End/js/companyOptions.js
+++ Front End/js/companyOptions.js
@@ -34,9 +34,70 @@ function deletePar(data, sect, id){
 		par.parentNode.removeChild(par);
 	}
 	else{
-		document.getElementById("error").textContent = "Error: " + data.message;
-		document.getElementById("error").className = "badStatus";
+		document.getElementById(sect + "Error").textContent = "Error: " + data.message;
+		document.getElementById(sect + "Error").className = "badStatus";
+		document.getElementById(sect + "Error").style.display = "block";
 	}
+}
+
+//Edit list
+function editList(response, name, entity){
+	var div = document.getElementById(entity + "List");
+	var par;
+	
+	if (response.status == "success" || response.active == 1){
+		getEntityId(entity, name);
+	}
+	else{
+		//No items, display error message
+		document.getElementById(entity + "Error").textContent = response.message;
+		document.getElementById(entity + "Error").className = "badStatus";
+		document.getElementById(entity + "Error").style.display = "block";
+	}
+}
+
+//Get id for entity
+function getEntityId(entity, name){
+	if (entity == "dept")
+		var queryStr = "divisions/";
+	else if (entity == "award")
+		var queryStr = "awards/";
+	else if (entity == "bonus")
+		var queryStr = "bonuses/";
+	makeRequestWithExtraParams('GET', "http://138.197.7.194/api/" + queryStr, null, false, true, handleEntityIdResponse, name, entity);
+}
+
+//Handle response for entity id
+function handleEntityIdResponse(response, name, entity){	
+	//Find id for name, format name, and create par
+	var id = -1;
+	var nameStr;
+	console.log("In handle entity id response, name = " + name + ", entity = " + entity);
+	console.log(response);
+	response.forEach(function(data){
+		if (entity == "dept"){
+			if (data.name.toLowerCase() == name.toLowerCase())
+				id = data.did;
+			nameStr = captialize(name);
+			par = createPar(entity, id, nameStr, deleteDeptReq);
+		}
+		else if (entity == "award"){
+			if (data.title.toLowerCase() == name.toLowerCase())
+				id = data.aid;
+			nameStr = captialize(name);
+			par = createPar(entity, id, nameStr, deleteAwardReq);
+		}
+		else if (entity == "bonus"){
+			if (data.amount == name)
+				id = data.bid;
+			nameStr = "$" + name;
+			par = createPar(entity, id, nameStr, deleteBonusReq);
+		}
+	});
+	
+	//Create new list item
+	var div = document.getElementById(entity + "List");
+	div.appendChild(par);
 }
 
 //Initialize page
@@ -48,10 +109,10 @@ function initializePage(){
 	}
 	else{
 		console.log("Logged in = true");
+		clearErrors();
 		makeRequest('GET', "http://138.197.7.194/api/divisions/", null, true, createDeptList);
 		makeRequest('GET', "http://138.197.7.194/api/awards/", null, true, createAwardList);
 		makeRequest('GET', "http://138.197.7.194/api/bonuses/", null, true, createBonusList);
-		clearErrors();
 	}
 }
 
@@ -62,21 +123,17 @@ function createDeptList(response){
 	var div = document.getElementById("deptList");
 	var par;
 	
-	if (response.length > 1){
+	if (response.length >= 1){
 		response.forEach(function(data){
 			par = createPar('dept', data.did, captialize(data.name), deleteDeptReq);
 			div.appendChild(par);
 		});
 	}
-	else if (response.did){
-		par = createPar('dept', response.did, captialize(response.name), deleteDeptReq);
-		div.appendChild(par);
-	}
 	else{
 		//No items, create message
-		par = document.createElement('p');
-		par.textContent = "There are no departments for your company, add some now!";
-		div.appendChild(par);
+		document.getElementById("deptError").textContent = "There are no departments for your company, add some now!";
+		document.getElementById("deptError").className = "goodStatus";
+		document.getElementById("deptError").style.display = "block";
 	}
 }
 
@@ -96,16 +153,13 @@ function deleteDeptReq(obj){
 		console.log(sect + " " + id);
 		makeRequestWithExtraParams('PUT', "http://138.197.7.194/api/divisions/", data, false, true, deletePar, sect, id);
 	}
-
-	document.getElementById("error").textContent = "";
 }
 
 //Add new dept to list
 function addDept(){
 	var data = {}
 	data.name = document.getElementById("dept").value;
-	makeRequest('POST', "http://138.197.7.194/api/divisions/", data, true, createDeptList);
-	document.getElementById("error").textContent = "";
+	makeRequestWithExtraParams('POST', "http://138.197.7.194/api/divisions/", data, true, true, editList, data.name, "dept");
 	document.getElementById("dept").value = "";
 	clearErrors();
 }
@@ -116,21 +170,17 @@ function createAwardList(response){
 	var div = document.getElementById("awardList");
 	var par;
 	
-	if (response.length > 1){
+	if (response.length >= 1){
 		response.forEach(function(data){
 			par = createPar('award', data.aid, data.title, deleteAwardReq);
 			div.appendChild(par);
 		});
 	}
-	else if (response.aid){
-		par = createPar('award', response.aid, response.title, deleteAwardReq);
-		div.appendChild(par);
-	}
 	else{
 		//No items, create message
-		par = document.createElement('p');
-		par.textContent = "There are no award types for your company, add some now!";
-		div.appendChild(par);
+		document.getElementById("awardError").textContent = "There are no award types for your company, add some now!";
+		document.getElementById("awardError").className = "goodStatus";
+		document.getElementById("awardError").style.display = "block";
 	}
 }
 
@@ -149,16 +199,13 @@ function deleteAwardReq(obj){
 		data.append("id", id);
 		makeRequestWithExtraParams('PUT', "http://138.197.7.194/api/awards/", data, false, true, deletePar, sect, id);
 	}
-
-	document.getElementById("error").textContent = "";
 }
 
 //Add new award to list
 function addAward(){
 	var data = {}
 	data.title = document.getElementById("award").value;
-	makeRequest('POST', "http://138.197.7.194/api/awards/", data, true, createAwardList);
-	document.getElementById("error").textContent = "";
+	makeRequestWithExtraParams('POST', "http://138.197.7.194/api/awards/", data, true, true, editList, data.title, "award");
 	document.getElementById("award").value = "";
 	clearErrors();
 }
@@ -170,21 +217,17 @@ function createBonusList(response){
 	var div = document.getElementById("bonusList");
 	var par;
 	
-	if (response.length > 1){
+	if (response.length >= 1){
 		response.forEach(function(data){
 			par = createPar('bonus', data.bid, "$" + data.amount, deleteBonusReq);
 			div.appendChild(par);
 		});
 	}
-	else if (response.bid){
-		par = createPar('bonus', response.bid, "$" + response.amount, deleteBonusReq);
-		div.appendChild(par);
-	}
 	else{
 		//No items, create message
-		par = document.createElement('p');
-		par.textContent = "There are no bonus amounts for your company, add some now!";
-		div.appendChild(par);
+		document.getElementById("bonusError").textContent = "There are no bonus amounts for your company, add some now!";
+		document.getElementById("bonusError").className = "goodStatus";
+		document.getElementById("bonusError").style.display = "block";
 	}
 }
 
@@ -203,8 +246,6 @@ function deleteBonusReq(obj){
 		data.append("id", id);
 		makeRequestWithExtraParams('PUT', "http://138.197.7.194/api/bonuses/", data, false, true, deletePar, sect, id);
 	}
-
-	document.getElementById("error").textContent = "";
 }
 
 //Add new bonus to list
@@ -214,18 +255,21 @@ function addBonus(){
 	data.amount = document.getElementById("bonus").value;
 	
 	if (data.amount > 0){
-		makeRequest('POST', "http://138.197.7.194/api/bonuses/", data, true, createBonusList);
-		document.getElementById("error").textContent = "";
+		makeRequestWithExtraParams('POST', "http://138.197.7.194/api/bonuses/", data, true, true, editList, data.amount, "bonus");
 		document.getElementById("bonus").value = "";
 	}
 	else
 		document.getElementById("bonusError").style.display = "block";
+		document.getElementById("bonusError").textContent = "Error: Please make sure the bonus amount is positive";
+		document.getElementById("bonusError").className = "badStatus";
 	
 }
 
 //Clear errors
 function clearErrors(){
 	document.getElementById("bonusError").style.display = "none";
+	document.getElementById("awardError").style.display = "none";
+	document.getElementById("deptError").style.display = "none";
 }
 
 
